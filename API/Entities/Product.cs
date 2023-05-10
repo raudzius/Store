92ce78cf1137fd41185ceb82dc3fbc44e7b944af
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-05-10 19:38:05 +0300

    fix: correct product entity class

--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -5,8 +5,9 @@ namespace API.Entities
     public int Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
+    public string? PictureUrl { get; set; }
     public long Price { get; set; }
-    public string? Title { get; set; }
+    public string? Type { get; set; }
     public string? Brand { get; set; }
     public int QuantityInStock { get; set; }
   }
