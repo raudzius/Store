c4e5b2b7e552696c29483cac65a9881b775c017e
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-05-10 22:18:01 +0300

    feat: add contact page

---
+++ client/src/features/contact/ContactPage.tsx
@@ -0,0 +1,6 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const ContactPage: React.FC = () => <Typography variant="h1">ContactPage</Typography>;
+
+export default ContactPage;
