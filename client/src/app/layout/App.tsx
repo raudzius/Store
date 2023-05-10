dd1ed15930e8241d7d6ddeced1462323233afc75
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-05-10 22:16:53 +0300

    feat: client can change theme

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
-import { Container, CssBaseline } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Container, CssBaseline, ThemeProvider, createTheme,
+} from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 
-const App: React.FC = () => (
-  <>
-    <CssBaseline />
-    <Header />
-    <Container>
-      <Outlet />
-    </Container>
-  </>
-);
+const App: React.FC = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === 'light' ? '#eaeaea' : '#121212',
+      },
+    },
+  });
+
+  const handleThemeChange = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Outlet />
+      </Container>
+    </ThemeProvider>
+  );
+};
 
 export default App;
