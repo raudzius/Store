58e8f878d27d14ad0f45b1d4f28e9fed4a383630
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-05-10 20:30:06 +0300

    style: configure eslint

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,14 +1,19 @@
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  extends: ['airbnb', 'airbnb-typescript'],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { project: 'client/tsconfig.json' },
   plugins: ['react-refresh'],
+  ignorePatterns: ['.eslintrc.cjs', 'vite.config.ts'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'react/require-default-props': [0],
   },
-}
+};
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-const App: React.FC = () => {
-  return <div>App</div>;
-};
+const App: React.FC = () => <div>App</div>;
 
 export default App;
