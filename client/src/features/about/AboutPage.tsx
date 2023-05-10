9d2717d8365d0ef4db90a7c24e9771b7a368dd53
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-05-10 22:17:47 +0300

    feat: add about page

---
+++ client/src/features/about/AboutPage.tsx
@@ -0,0 +1,6 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const AboutPage: React.FC = () => <Typography variant="h1">AboutPage</Typography>;
+
+export default AboutPage;
