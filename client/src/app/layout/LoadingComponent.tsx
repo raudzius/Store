cc20c3afc6972a4f2905cacfbd57bcfdc6fec920
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-05-10 22:09:32 +0300

    feat: add loading component

---
+++ client/src/app/layout/LoadingComponent.tsx
@@ -0,0 +1,21 @@
+import {
+  Backdrop, Box, CircularProgress, Typography,
+} from '@mui/material';
+import React from 'react';
+
+type Props = {
+  message?: string;
+};
+
+const LoadingComponent: React.FC<Props> = ({ message = 'Loading...' }) => (
+  <Backdrop open invisible>
+    <Box display="flex" justifyContent="center" alignItems="center" height="100vh">
+      <CircularProgress size={100} color="secondary" />
+      <Typography variant="h4" sx={{ justifyContent: 'center', position: 'fixed', top: '60%' }}>
+        {message}
+      </Typography>
+    </Box>
+  </Backdrop>
+);
+
+export default LoadingComponent;
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,11 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductList from './ProductList';
 import agent from '../../app/api/agent';
+import LoadingComponent from '../../app/layout/LoadingComponent';
 
 const Catalog: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  agent.Catalog.list().then((productsData) => setProducts(productsData));
+  useEffect(() => {
+    agent.Catalog.list()
+      .then((productsData) => {
+        setProducts(productsData);
+      })
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <LoadingComponent message="Loading products..." />;
 
   return <ProductList products={products} />;
 };
