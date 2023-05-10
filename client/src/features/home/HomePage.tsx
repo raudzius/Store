a5b61c23540b3bad1c431c634f1ae7fbbef4d063
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-05-10 22:18:19 +0300

    feat: add home page

---
+++ client/src/features/home/HomePage.tsx
@@ -0,0 +1,6 @@
+import { Typography } from '@mui/material';
+import React from 'react';
+
+const HomePage: React.FC = () => <Typography variant="h1">HomePage</Typography>;
+
+export default HomePage;
