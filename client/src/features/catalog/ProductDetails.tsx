6d9ec8e368d11b97dd8d604cae0609c100adca23
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-05-10 22:21:31 +0300

    feat: create product details page

---
+++ client/src/features/catalog/ProductDetails.tsx
@@ -0,0 +1,73 @@
+import {
+  Divider,
+  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import axios from 'axios';
+import Image from 'mui-image';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const ProductDetails: React.FC = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState<Product | null>(null);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/products/${id}`)
+      .then((response) => setProduct(response.data))
+      .catch((error) => console.log(error))
+      .finally(() => setLoading(false));
+  }, [id]);
+
+  if (loading) return <Typography variant="h3">Loading...</Typography>;
+  if (!product) return <Typography variant="h3">Product not found</Typography>;
+
+  return (
+    <Grid container spacing={6}>
+      <Grid item xs={6}>
+        <Image src={product.pictureUrl} alt={product.name} width="100%" duration={0} />
+      </Grid>
+      <Grid item xs={6}>
+        <Typography variant="h3">{product.name}</Typography>
+        <Divider sx={{ mb: 2 }} />
+        <Typography variant="h4" color="secondary">
+          {`€${(product.price / 100).toFixed(2)}`}
+        </Typography>
+        <TableContainer>
+          <Table>
+            <TableBody>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>{product.name}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Description</TableCell>
+                <TableCell>{product.description}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Type</TableCell>
+                <TableCell>{product.type}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Brand</TableCell>
+                <TableCell>{product.brand}</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Quantity in stock</TableCell>
+                <TableCell>{product.quantityInStock}</TableCell>
+              </TableRow>
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default ProductDetails;
