cc61e10df5a2e897005d25127b3238a255d017d1
Mykolas Raudzius <raudziusmykolas@gmail.com>
2023-05-14 22:30:46 +0300

    feat: test error responses using client

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,6 +1,47 @@
-import { Typography } from '@mui/material';
+import {
+  Button, ButtonGroup, Container, Typography,
+} from '@mui/material';
 import React from 'react';
+import agent from '../../app/api/agent';
 
-const AboutPage: React.FC = () => <Typography variant="h1">AboutPage</Typography>;
+const AboutPage: React.FC = () => (
+  <Container>
+    <Typography gutterBottom variant="h2">
+      Errors for testing purposes
+    </Typography>
+    <ButtonGroup fullWidth>
+      <Button
+        variant="contained"
+        onClick={() => agent.TestErrors.get400Error().catch((error) => console.log(error))}
+      >
+        Test 400 Error
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => agent.TestErrors.get401Error().catch((error) => console.log(error))}
+      >
+        Test 401 Error
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => agent.TestErrors.get404Error().catch((error) => console.log(error))}
+      >
+        Test 404 Error
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => agent.TestErrors.get500Error().catch((error) => console.log(error))}
+      >
+        Test 500 Error
+      </Button>
+      <Button
+        variant="contained"
+        onClick={() => agent.TestErrors.getValidationError().catch((error) => console.log(error))}
+      >
+        Test Validation Error
+      </Button>
+    </ButtonGroup>
+  </Container>
+);
 
 export default AboutPage;
